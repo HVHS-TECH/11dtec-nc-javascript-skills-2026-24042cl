e0d852d58344c4c047998b52a14c836a57c66fa6
24042cl <24042cl@hvhs.school.nz>
2026-04-29 02:44:54 +0000

    Apr 29, 2026, 2:44 AM

--- t03_maths.js
+++ t03_maths.js
@@ -3,18 +3,23 @@ Name of task: Math Equations
 ****************************/
 let num1 = 10;
 let num2 = 5;
+let answer;
 
 console.log("add",     num1 + num2);
-console.log("add",     num1 + num2);
-
+console.log("subtract",     num1 - num2);
+console.log("multiply",     num1 * num2);
+console.log("divide",     num1 / num2);
 /****************************
 Main code
 ****************************/
-
-
-
-
-
+answer = num1 + num2;
+console.log("add",     answer);
+answer = num1 - num2;
+console.log("subtract",     answer);
+answer = num1 * num2;
+console.log("multiply",     answer);
+answer = num1 / num2;
+console.log("divide",     answer);
 /****************************
 Functions
 ****************************/
