2747ce70b0ad3847d3168ada7a2c3cc7c9724ead
24042cl <24042cl@hvhs.school.nz>
2026-04-29 02:16:11 +0000

    Apr 29, 2026, 2:16 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,28 @@
-/* Paste your code from task 1 here */
+/****************************
+This is a block comment
+I am a heaer comment
+****************************/
+
+let Username = "charles";
+let year = "2026";
+let age = 16;
+let money = 67
+console.log("Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars")
+/****************************
+Name of task: Intro to Javascript
+****************************/
+console.log("Running t01_introduction.js")
+console.log("hello boi");
+
+//Variables
+
+/****************************
+Main code
+****************************/
+
+
+
+
+/****************************
+Functions
+****************************/
