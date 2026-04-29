6ee0dbd29be578d68f49891b2e990ced25868949
24042cl <24042cl@hvhs.school.nz>
2026-04-29 02:32:05 +0000

    Apr 29, 2026, 2:32 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,27 @@
-/* Paste your code from task 2 here */
+/****************************
+This is a block comment
+****************************/
+
+let Username = "charles";
+let year = "2026";
+let age = 16;
+let money = 67
+console.log("Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars")
+/****************************
+Name of task: Intro to Javascript
+****************************/
+console.log("Running t01_introduction.js")
+console.log("hello boi");
+
+//Variables
+
+/****************************
+Main code
+****************************/
+
+
+
+
+/****************************
+Functions
+****************************/
