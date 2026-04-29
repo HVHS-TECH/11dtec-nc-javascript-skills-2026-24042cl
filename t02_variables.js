e37d111f447054aa55b2689537ed74c03e325ac6
24042cl <24042cl@hvhs.school.nz>
2026-04-29 02:35:01 +0000

    Apr 29, 2026, 2:35 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,25 +1,19 @@
 /****************************
-This is a block comment
-I am a heaer comment
+Name of task 
 ****************************/
 
+//Variables
 let Username = "charles";
 let year = "2026";
 let age = 16;
 let money = 67
-console.log("Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars")
-/****************************
-Name of task: Intro to Javascript
-****************************/
-console.log("Running t01_introduction.js")
-console.log("hello boi");
 
-//Variables
+
 
 /****************************
 Main code
 ****************************/
-
+console.log("Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars")
 
 
 
