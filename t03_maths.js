51fca6c7529febc6896bcdcf54d5010499ed7903
24042cl <24042cl@hvhs.school.nz>
2026-04-29 02:56:51 +0000

    Apr 29, 2026, 2:56 AM

--- t03_maths.js
+++ t03_maths.js
@@ -21,6 +21,7 @@ console.log("You were born in " + birthYear);
 console.log("In 10 years you will be " + oldAge + " years old.");
 console.log("You have " + money + " dollars");
 console.log("You have spent half of your money, you now have " + halfMoney + " dollars left.");
+console.log("The you get $3, now you have " + halfMoney + " dollars.");
 /****************************
 Functions
 ****************************/
