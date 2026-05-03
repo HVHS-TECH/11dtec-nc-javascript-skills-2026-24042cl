a1eb96bc12c3db17854ca01098a8766ee717157d
24042cl <24042cl@hvhs.school.nz>
2026-05-03 23:00:31 +0000

    May 3, 2026, 11:00 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -2,7 +2,6 @@
 Name of task: JavaScript and HTML
 ****************************/
 console.log("Running t01_javascriptAndHtml.js")
-console.log("hello boi");
 
 //Variables
 let Username = "charles";
@@ -15,18 +14,18 @@ oldAge = age + 10;
 birthYear = year - age;
 halfMoney = money / 2;
 add3 = money / 2 + 3;
-const OUTPUT = document.getElementById("spaceForJavaSciptOutput")
-OUTPUT.innerHTML = "<h2>Added by JavaScipt</h2>"
+
 /****************************
 Main code
 ****************************/
-
-OUTPUT.innerHTML += ("<p>Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars,</p>");
-OUTPUT.innerHTML += ("<p>You were born in " + birthYear +"</p>");
-OUTPUT.innerHTML += ("<p>In 10 years you will be " + oldAge + " years old.</p>");
-OUTPUT.innerHTML += ("<p>You have " + money + " dollars</p>");
-OUTPUT.innerHTML += ("<p>You have spent half of your money, you now have " + halfMoney + " dollars left.</p>");
-OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + add3 + " dollars.</p>");
+const OUTPUT = document.getElementById("spaceForJavaSciptOutput");
+OUTPUT.innerHTML = "<h2>Added by JavaScipt</h2>";
+OUTPUT.innerHTML += "<p>Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars,</p>";
+OUTPUT.innerHTML += "<p>You were born in " + birthYear +"</p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + " years old.</p>";
+OUTPUT.innerHTML += "<p>You have " + money + " dollars</p>";
+OUTPUT.innerHTML += "<p>You have spent half of your money, you now have " + halfMoney + " dollars left.</p>";
+OUTPUT.innerHTML += "<p>Then you get $3, now you have " + add3 + " dollars.</p>";
 
 
 /****************************
