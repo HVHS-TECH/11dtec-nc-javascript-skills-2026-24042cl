6f96666639d1e5f10068ddd1bbd356f950e8ed1f
24042cl <24042cl@hvhs.school.nz>
2026-05-03 22:40:35 +0000

    May 3, 2026, 10:40 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -15,18 +15,17 @@ oldAge = age + 10;
 birthYear = year - age;
 halfMoney = money / 2;
 add3 = money / 2 + 3;
-
+const OUTPUT = document.getElementById("spaceForJavaSciptOutput")
+OUTPUT.innerHTML = "<h2>Added by JavaScipt</h2>"
 /****************************
 Main code
 ****************************/
-const OUTPUT = document.getElementById("spaceForJavaSciptOutput")
-OUTPUT.innerHTML = "<h2>Added by JavaScipt</h2>"
-console.log("Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars");
-console.log("You were born in " + birthYear);
-console.log("In 10 years you will be " + oldAge + " years old.");
-console.log("You have " + money + " dollars");
-console.log("You have spent half of your money, you now have " + halfMoney + " dollars left.");
-console.log("Then you get $3, now you have " + add3 + " dollars.");
+OUTPUT.innerHTML += ("Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars");
+OUTPUT.innerHTML += ("You were born in " + birthYear);
+OUTPUT.innerHTML += ("In 10 years you will be " + oldAge + " years old.");
+OUTPUT.innerHTML += ("You have " + money + " dollars");
+OUTPUT.innerHTML += ("You have spent half of your money, you now have " + halfMoney + " dollars left.");
+OUTPUT.innerHTML += ("Then you get $3, now you have " + add3 + " dollars.");
 /****************************
 Functions
 ****************************/
