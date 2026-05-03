5ef8614ba48a69f892656a54553a30c1d9e799e4
24042cl <24042cl@hvhs.school.nz>
2026-05-03 22:44:01 +0000

    May 3, 2026, 10:44 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -20,12 +20,12 @@ OUTPUT.innerHTML = "<h2>Added by JavaScipt</h2>"
 /****************************
 Main code
 ****************************/
-OUTPUT.innerHTML += ("Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars");
-OUTPUT.innerHTML += ("You were born in " + birthYear);
-OUTPUT.innerHTML += ("In 10 years you will be " + oldAge + " years old.");
-OUTPUT.innerHTML += ("You have " + money + " dollars");
-OUTPUT.innerHTML += ("You have spent half of your money, you now have " + halfMoney + " dollars left.");
-OUTPUT.innerHTML += ("Then you get $3, now you have " + add3 + " dollars.");
+OUTPUT.innerHTML += ("<p>Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars,</p>");
+OUTPUT.innerHTML += ("<p>You were born in " + birthYear +);
+OUTPUT.innerHTML += ("<p>In 10 years you will be " + oldAge + " years old.</p>");
+OUTPUT.innerHTML += ("<p>You have " + money + " dollars</p>");
+OUTPUT.innerHTML += ("<p>You have spent half of your money, you now have " + halfMoney + " dollars left.</p>");
+OUTPUT.innerHTML += ("<p>Then you get $3, now you have " + add3 + " dollars.</p>");
 /****************************
 Functions
 ****************************/
