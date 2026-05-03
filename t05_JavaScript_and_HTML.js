9744a28bb373bc06c5ee1ea1db8a86e1e4e32ffa
24042cl <24042cl@hvhs.school.nz>
2026-05-03 22:29:21 +0000

    May 3, 2026, 10:29 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,28 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+Name of task: JavaScript and HTML
+****************************/
+
+//Variables
+let Username = "charles";
+let year = "2026";
+let age = 16;
+let money = 67
+let birthYear;
+let oldAge;
+oldAge = age + 10;
+birthYear = year - age;
+halfMoney = money / 2;
+add3 = money / 2 + 3;
+
+/****************************
+Main code
+****************************/
+console.log("Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars");
+console.log("You were born in " + birthYear);
+console.log("In 10 years you will be " + oldAge + " years old.");
+console.log("You have " + money + " dollars");
+console.log("You have spent half of your money, you now have " + halfMoney + " dollars left.");
+console.log("Then you get $3, now you have " + add3 + " dollars.");
+/****************************
+Functions
+****************************/
