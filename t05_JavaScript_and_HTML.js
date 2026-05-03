d76d285bbeef7b17582c46e599ac921bc4f43b48
24042cl <24042cl@hvhs.school.nz>
2026-05-03 22:36:38 +0000

    May 3, 2026, 10:36 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -19,7 +19,8 @@ add3 = money / 2 + 3;
 /****************************
 Main code
 ****************************/
-const OUTPUT = document.
+const OUTPUT = document.getElementById("spaceForJavaSciptOutput")
+OUTPUT.innerHTML = "<h2>Added by Java"
 console.log("Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars");
 console.log("You were born in " + birthYear);
 console.log("In 10 years you will be " + oldAge + " years old.");
