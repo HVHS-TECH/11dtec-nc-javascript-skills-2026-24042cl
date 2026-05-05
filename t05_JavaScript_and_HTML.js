958b5bab2b749502542b3171102ea8416ee4beb0
24042cl <24042cl@hvhs.school.nz>
2026-05-05 00:21:21 +0000

    May 5, 2026, 12:21 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -20,12 +20,12 @@ Main code
 ****************************/
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 OUTPUT.innerHTML = "<h2>Added by JavaScript.</h2>";
-/*OUTPUT.innerHTML += "<p>Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars,</p>";
+OUTPUT.innerHTML += "<p>Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars,</p>";
 OUTPUT.innerHTML += "<p>You were born in " + birthYear +"</p>";
 OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + " years old.</p>";
 OUTPUT.innerHTML += "<p>You have " + money + " dollars</p>";
 OUTPUT.innerHTML += "<p>You have spent half of your money, you now have " + halfMoney + " dollars left.</p>";
-OUTPUT.innerHTML += "<p>Then you get $3, now you have " + add3 + " dollars.</p>";*/
+OUTPUT.innerHTML += "<p>Then you get $3, now you have " + add3 + " dollars.</p>";
 
 
 /****************************
--- t06_functions.js
+++ t06_functions.js
@@ -1,1 +1,33 @@
-/* Paste your code from task 5 here */
+/****************************
+Name of task: JavaScript and HTML
+****************************/
+console.log("Running t01_javascriptAndHtml.js")
+
+//Variables
+let Username = "charles";
+let year = "2026";
+let age = 16;
+let money = 67
+let birthYear;
+let oldAge;
+oldAge = age + 10;
+birthYear = year - age;
+halfMoney = money / 2;
+add3 = money / 2 + 3;
+
+/****************************
+Main code
+****************************/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by JavaScript.</h2>";
+OUTPUT.innerHTML += "<p>Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars,</p>";
+OUTPUT.innerHTML += "<p>You were born in " + birthYear +"</p>";
+OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + " years old.</p>";
+OUTPUT.innerHTML += "<p>You have " + money + " dollars</p>";
+OUTPUT.innerHTML += "<p>You have spent half of your money, you now have " + halfMoney + " dollars left.</p>";
+OUTPUT.innerHTML += "<p>Then you get $3, now you have " + add3 + " dollars.</p>";
+
+
+/****************************
+Functions
+****************************/
