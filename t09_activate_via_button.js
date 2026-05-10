84e0ac2fdea4b3b546bd376d441daeac651daa5d
24042cl <24042cl@hvhs.school.nz>
2026-05-10 22:49:48 +0000

    May 10, 2026, 10:49 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -45,7 +45,3 @@ function displayWelcome(_name, _age){
     OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
     OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
 }
-Open the console <code>(Ctrl-shift-i)</code>
-<button onClick="start()">START</button>
-
-<div id="spaceForJavaScript">
