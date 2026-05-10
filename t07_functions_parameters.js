fe23a03340e0b16f7e254b4b1222acee4a344af8
24042cl <24042cl@hvhs.school.nz>
2026-05-10 09:13:14 +0000

    May 10, 2026, 9:13 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -27,11 +27,17 @@ OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + " years old.</p>";
 OUTPUT.innerHTML += "<p>You have " + money + " dollars</p>";
 OUTPUT.innerHTML += "<p>You have spent half of your money, you now have " + halfMoney + " dollars left.</p>";
 OUTPUT.innerHTML += "<p>Then you get $3, now you have " + add3 + " dollars.</p>";
+displayWelcome("Mr bob");
 
 
 /****************************
 Functions
 ****************************/
+function writeLine(){
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+    // add a line on the html page
+    OUTPUT.innerHTML += "<p>Less code matters</p>"
+}
 function displayWelcome(_name){
     OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
 }
