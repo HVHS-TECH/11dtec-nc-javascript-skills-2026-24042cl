4fbf783aa92b7191ff0ef6e8c69c25170424ced1
24042cl <24042cl@hvhs.school.nz>
2026-05-13 02:51:45 +0000

    May 13, 2026, 2:51 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -20,34 +20,8 @@ currentYear = 2026
 /****************************
 Main code
 ****************************/
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+const NAME_FIELD = document.getElementById("nameField");
+let userName = NAME_FIELD.value;
 /****************************
 Functions
 ****************************/
-function writeLine(){
-    const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
-    // add a line on the html page
-    OUTPUT.innerHTML += "<p>Less code matters</p>"
-}
-function displayWelcome(_name){
-    OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
-}
-function displayWelcome(_name, _age){
-    OUTPUT.innerHTML += "<p>Welcome to the page "+_name+"</p>";
-    OUTPUT.innerHTML += "<p>You are "+_age+" years old</p>";
-}
-function start() {
-    OUTPUT.innerHTML = "<h2>Added by JavaScript.</h2>";
-    writeLine()
-OUTPUT.innerHTML += "<p>Hi " + Username + " as of " + year + " you are " + age + " years old. You have " + money + " dollars,</p>";
-OUTPUT.innerHTML += "<p>You were born in " + birthYear +"</p>";
-OUTPUT.innerHTML += "<p>In 10 years you will be " + oldAge + " years old.</p>";
-OUTPUT.innerHTML += "<p>You have " + money + " dollars</p>";
-OUTPUT.innerHTML += "<p>You have spent half of your money, you now have " + halfMoney + " dollars left.</p>";
-OUTPUT.innerHTML += "<p>Then you get $3, now you have " + add3 + " dollars.</p>";
-displayWelcome("charles" , 16);
-    OUTPUT.innerHTML
-    OUTPUT.innerHTML
-    answer = currentYear - age
-    OUTPUT.innerHTML
-}
