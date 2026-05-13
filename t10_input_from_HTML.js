875c0f5784017edf4ddcc5a0c5fb1e5fd515604b
24042cl <24042cl@hvhs.school.nz>
2026-05-13 02:58:37 +0000

    May 13, 2026, 2:58 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -4,18 +4,6 @@ Name of task: Input From HTML
 console.log("Running t01_javascriptAndHtml.js")
 
 //Variables
-let Username = "charles";
-let year = "2026";
-let age = 16;
-let money = 67
-let birthYear;
-let oldAge;
-oldAge = age + 10;
-birthYear = year - age;
-halfMoney = money / 2;
-add3 = money / 2 + 3;
-let currentYear
-currentYear = 2026
 
 /****************************
 Main code
