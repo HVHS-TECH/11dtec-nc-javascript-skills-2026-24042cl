79a99105ca4c33f5127eaa9ff6606db8dd38f1f3
24042cl <24042cl@hvhs.school.nz>
2026-05-14 21:19:39 +0000

    May 14, 2026, 9:19 PM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,1 +1,20 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Input From HTML
+****************************/
+console.log("Running t01_javascriptAndHtml.js")
+
+//Variables
+
+/****************************
+Main code
+****************************/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+/****************************
+Functions
+****************************/
+function getFormInput(){
+ const NAME_FIELD = document.getElementById("nameField");
+ let userName = NAME_FIELD.value;
+ OUTPUT.innerHTML = "<p> Your name is "+userName+"</p>"
+}
