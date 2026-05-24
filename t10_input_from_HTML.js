4b13d363ba699e9da1df0f3ed0fca89411cd3609
24042cl <24042cl@hvhs.school.nz>
2026-05-24 22:38:25 +0000

    May 24, 2026, 10:38 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -14,11 +14,6 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 Functions
 ****************************/
 function getFormInput(){
-const AGE_FIELD = document.getElementById("ageField");
- let age = AGE_FIELD.value;
- OUTPUT.innerHTML = "<p> You are "+age+" years old"+"</p>"
-}
-function getFormInput(){
  const NAME_FIELD = document.getElementById("nameField");
  let userName = NAME_FIELD.value;
  OUTPUT.innerHTML = "<p> Your name is "+userName+"</p>"
