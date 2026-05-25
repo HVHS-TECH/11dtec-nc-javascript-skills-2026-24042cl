ccd8e97bd6e3ae273c0a67905efa9b677cae73ae
24042cl <24042cl@hvhs.school.nz>
2026-05-25 23:37:04 +0000

    May 25, 2026, 11:37 PM

--- t11_data_types.js
+++ t11_data_types.js
@@ -21,4 +21,8 @@ function getFormInput(){
  const AGE_FIELD = document.getElementById("ageField");
  let age = AGE_FIELD.value;
  OUTPUT.innerHTML += "<p> You are "+age+" years old"+"</p>"
+ 
+ const MONEY_FIELD = document.getElementById("moneyField");
+ let money = MONEY_FIELD.value;
+ OUTPUT.innerHTML += "<p> You have "+age+" dollars"+"</p>"
 }
