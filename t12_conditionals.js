0d7a45c4158cc95432842d0e1c9d9867ece36a10
24042cl <24042cl@hvhs.school.nz>
2026-05-25 23:38:50 +0000

    May 25, 2026, 11:38 PM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,1 +1,28 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Data Types
+****************************/
+console.log("Running t01_javascriptAndHtml.js")
+
+//Variables
+
+/****************************
+Main code
+****************************/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+/****************************
+Functions
+****************************/
+function getFormInput(){
+ const NAME_FIELD = document.getElementById("nameField");
+ let userName = NAME_FIELD.value;
+ OUTPUT.innerHTML = "<p> Your name is "+userName+"</p>"
+ 
+ const AGE_FIELD = document.getElementById("ageField");
+ let age = AGE_FIELD.value;
+ OUTPUT.innerHTML += "<p> You are "+age+" years old"+"</p>"
+ 
+ const MONEY_FIELD = document.getElementById("moneyField");
+ let money = MONEY_FIELD.value;
+ OUTPUT.innerHTML += "<p> You have "+age+" dollars"+"</p>"
+}
