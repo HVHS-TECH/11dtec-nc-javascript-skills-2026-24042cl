3d23972212b4d52a3348ac10a4f7ce2068d8c33f
24042cl <24042cl@hvhs.school.nz>
2026-05-31 02:29:35 +0000

    May 31, 2026, 2:29 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,1 +1,20 @@
-/* Paste your code from the last task */
+/****************************
+Name of task: Intro to Javascript
+****************************/
+console.log("Running t01_introduction.js")
+console.log("hello boi");
+
+//Variables
+
+/****************************
+Main code
+****************************/
+for (let i = 0; i < 10; i++){
+    OUTPUT.innerHTML += "<p>i is currently "+i+"</p>"
+}
+
+
+
+/****************************
+Functions
+****************************/
