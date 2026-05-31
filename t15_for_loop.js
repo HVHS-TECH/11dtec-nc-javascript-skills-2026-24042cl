995dd0f58b008c22dcaa39554a9785b0a096cb8b
24042cl <24042cl@hvhs.school.nz>
2026-05-31 02:46:03 +0000

    May 31, 2026, 2:46 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -24,13 +24,6 @@ for (let i = 99; i >= 0; i--) {
 
   OUTPUT.innerHTML += `<p>${currentBottles} ${bottleWord} of milk on the wall, ${currentBottles} ${bottleWord} of milk.<br>`;
   OUTPUT.innerHTML += `Take one down, pass it around, ${nextBottles} ${nextBottleWord} of milk on the wall.</p><br><br>`;
-  if (i > 0) {
-    output += `${currentBottles} ${bottleWord} of milk on the wall, ${currentBottles} ${bottleWord} of milk.<br>`;
-    output += `Take one down, pass it around, ${nextBottles} ${nextBottleWord} of milk on the wall.<br><br>`;
-  } else {
-    output += `No more bottles of milk on the wall, no more bottles of milk.<br>`;
-    output += `Go to the store and buy some more, 99 bottles of milk on the wall!`;
-  }
 }
 
 container.innerHTML = output;
