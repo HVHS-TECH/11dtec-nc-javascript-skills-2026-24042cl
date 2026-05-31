b309a768fe382c2736cef625f3a00ca181f91234
24042cl <24042cl@hvhs.school.nz>
2026-05-31 08:26:18 +0000

    May 31, 2026, 8:26 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -11,17 +11,17 @@ console.log("hello boi");
 Main code
 ****************************/
 const container = document.getElementById("spaceForJavaScriptOutput");
-<scipt>
+<script>
   function singsong(){
-    let bottles = phraseInt(document.getElementById("bottleField").value);
+    let bottles = parseInt(document.getElementById("bottleField").value);
     let song = "";
 
     for (let i = bottles; i > 0; i--){
-      if {i > 1} {
-        song += i + " bottles of milk on the wall! " + i + " bottles of milk! 
+      if (i > 1) {
+        song += i + " bottles of milk on the wall! " + i + " bottles of milk! ";
         song += "Take one down, pass it around... " + (i-1) + " bottles of milk on the wall!\n";
       } else {
-        song += "1 bottle of milk on the wall! 1 bottle of milk!"
+        song += "1 bottle of milk on the wall! 1 bottle of milk!";
         song += "Take one down, pass it around... No more bottles of milk on the wall!\n";
       }
     }
