3e301213f19af357e7215cd8bf8ce5a2c1389458
24042cl <24042cl@hvhs.school.nz>
2026-05-31 02:33:39 +0000

    May 31, 2026, 2:33 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -5,12 +5,13 @@ console.log("Running t01_introduction.js")
 console.log("hello boi");
 
 //Variables
+let milk
 
 /****************************
 Main code
 ****************************/
 for (let i = 0; i < 10; i++){
-    OUTPUT.innerHTML += "<p>"99 bottles of <milk> on the wall "+i+"</p>"
+    OUTPUT.innerHTML += "<p>"99 bottles of "+ milk +" on the wall "+i+"</p>"
 }
 
 
