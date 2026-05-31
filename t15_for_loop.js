cc86adb7b1185f0a3fd843b2d5b677cd2b3441e2
24042cl <24042cl@hvhs.school.nz>
2026-05-31 08:16:26 +0000

    May 31, 2026, 8:16 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -12,18 +12,7 @@ Main code
 ****************************/
 const container = document.getElementById("spaceForJavaScriptOutput");
 
-for (let i = 99; i >= 0; i--) {
-  let currentBottles = i === 0 ? "No more" : i;
-  let nextBottles = i - 1 === 0 ? "no more" : (i - 1 < 0 ? 99 : i - 1);
-  let bottleWord = i === 1 ? "bottle" : "bottles";
-  let nextBottleWord = (i - 1) === 1 ? "bottle" : "bottles";
 
-  const BOTTLE_FIELD = document.getElementById("bottleField");
-  let bottles = parseInt(BOTTLE_FIELD.value) || 99;
-
-  OUTPUT.innerHTML += `<p>${currentBottles} ${bottleWord} of milk on the wall, ${currentBottles} ${bottleWord} of milk.<br>`;
-  OUTPUT.innerHTML += `Take one down, pass it around, ${nextBottles} ${nextBottleWord} of milk on the wall.</p><br><br>`;
-}
 
 
 
