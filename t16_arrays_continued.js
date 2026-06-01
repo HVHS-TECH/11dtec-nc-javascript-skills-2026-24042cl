63bc76f022f9dee8dc56f520d601a5ad89006eb0
24042cl <24042cl@hvhs.school.nz>
2026-06-01 06:36:19 +0000

    Jun 1, 2026, 6:36 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -8,7 +8,7 @@ console.log("Running t01_javascriptAndHtml.js")
  let age
  let money
  let change
- let shoppingCart = ["chocolate test", "drink test"];
+ let shoppingCart = [""];
 let chocolate
 let chips
 let drink
@@ -76,7 +76,7 @@ shoppingCart.push(drink);
 }
 function showCart(){
 for (let i=0; i < shoppingCart.length; i++){
-    OUTPUT.innerHTML = +shoppingCart[i]+"<br>"
+    OUTPUT.innerHTML += "<br>"+shoppingCart[i]+"<br>"
 }
 }
 
