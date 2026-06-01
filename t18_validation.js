fdcfc224761daf064f14f6e209a55e2b323c5c34
24042cl <24042cl@hvhs.school.nz>
2026-06-01 10:19:32 +0000

    Jun 1, 2026, 10:19 AM

--- t18_validation.js
+++ t18_validation.js
@@ -1,125 +1,161 @@
 /****************************
 Name of task: Validation
 ****************************/
-console.log("Running t01_javascriptAndHtml.js")
+console.log("Running t01_javascriptAndHtml.js");
 
-//Variables
- let userName
- let age
- let money
- let change
- let shoppingCart = [""];
-let chocolate
-let chips
-let drink
+// Variables
+let userName;
+let age;
+let money;
+let change;
+let shoppingCart = [];
 
 /****************************
 Main code
 ****************************/
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-displayWelcome(4 , 3 , 2.50);
+
+displayWelcome(4, 3, 2.50);
+
 /****************************
 Functions
 ****************************/
-function getFormInput(){
+
+function getFormInput() {
+
+    OUTPUT.innerHTML = "";
+
+    // NAME
     const NAME_FIELD = document.getElementById("nameField");
-    if (NAME_FIELD.checkValidity()=== false)  {
-    OUTPUT.innerHTML = "please fill out the form correctly"
-}else{
-userName = NAME_FIELD.value;
- OUTPUT.innerHTML = "<p> Your name is "+userName+"</p>"
-}
-const AGE_FIELD = document.getElementById("ageField");
-if (AGE_FIELD.value < 0){
-    OUTPUT.innerHTML = "how are you alive??"
-} else if (AGE_FIELD.value > 100){
-    OUTPUT.innerHTML = "you are probably not alive anymore??"
-} else if (AGE_FIELD.value == 0){
-    OUTPUT.innerHTML = "you are a newborn??"
-} else if (AGE_FIELD.value == 100){
-    OUTPUT.innerHTML = "congrats on being alive for a century!!"
-}
-if (AGE_FIELD.checkValidity()=== false)  {
-    OUTPUT.innerHTML = "please fill out the form correctly"
-}else{
-age = AGE_FIELD.value;
- OUTPUT.innerHTML += "<p> You are "+age+" years old"+"</p>"
-}
- 
- const MONEY_FIELD = document.getElementById("moneyField");
-if (MONEY_FIELD.value < 0){
-    OUTPUT.innerHTML = "you can't have negative money!!"
-} else if (MONEY_FIELD.value > 1000){
-    OUTPUT.innerHTML = "you are a millionaire!!"
-} else if (MONEY_FIELD.value == 0){
-    OUTPUT.innerHTML = "you are broke!!"
-} else if (MONEY_FIELD.value == 1000){
-    OUTPUT.innerHTML = "congrats on being a millionaire!!"
-}
-if (MONEY_FIELD.checkValidity()=== false)  {
-    OUTPUT.innerHTML = "please fill out the form correctly"
-}else{
-money = MONEY_FIELD.value;
- OUTPUT.innerHTML += "<p> You have "+money+" dollars"+"</p>"
-}
- displayWelcome(4 , 3 , 2.50);
 
- if (money > 4){
-    OUTPUT.innerHTML += "<p>a chocolate bar costs $4.</p>"
- }else{
-    OUTPUT.innerHTML += "<p>you CAN'T afford a chocolate bar.</p>"
- }
- 
-if (money < 4){
-    OUTPUT.innerHTML += "<p>a chocolate bar costs $4.</p>"
-}else{
-    OUTPUT.innerHTML += "<p>you CAN afford a chocolate bar.</p>"
-}
-const CHANGE_FIELD = document.getElementById("changeField");
- change = money - 4;
- CHANGE_FIELD.value = change;
- OUTPUT.innerHTML += "<p>Your change is $"+change+"</p>"
+    if (NAME_FIELD.checkValidity() === false) {
+        OUTPUT.innerHTML = "Please fill out the form correctly.";
+        return;
+    }
 
- const CHOCOLATE_FIELD = document.getElementById("chocolateField");
- let chocolateRating = ["You loathe chocolate", "Chocolate is meh", "Chocolate is pretty good", "Chocolate is the best thing EVER!!!!"];
+    userName = NAME_FIELD.value;
+    OUTPUT.innerHTML += "<p>Your name is " + userName + "</p>";
 
- let choice = CHOCOLATE_FIELD.value;
+    // AGE
+    const AGE_FIELD = document.getElementById("ageField");
 
- OUTPUT.innerHTML += "<p>"+chocolateRating[choice]+"</p>"
+    if (AGE_FIELD.checkValidity() === false) {
+        OUTPUT.innerHTML += "<p>Please enter a valid age.</p>";
+        return;
+    }
 
+    age = Number(AGE_FIELD.value);
 
-function displayWelcome(chocolate, chips, drink){
-    OUTPUT.innerHTML += "<p>Chocolate bar: $"+ chocolate +"</p>";
-    OUTPUT.innerHTML += "<p>Chips: $"+ chips +" </p>";
-    OUTPUT.innerHTML += "<p>Drink: $"+ drink +" </p>";
+    if (age < 0) {
+        OUTPUT.innerHTML += "<p>How are you alive??</p>";
+    } else if (age > 100) {
+        OUTPUT.innerHTML += "<p>You are probably not alive anymore??</p>";
+    } else if (age === 0) {
+        OUTPUT.innerHTML += "<p>You are a newborn??</p>";
+    } else if (age === 100) {
+        OUTPUT.innerHTML += "<p>Congrats on being alive for a century!!</p>";
+    }
+
+    OUTPUT.innerHTML += "<p>You are " + age + " years old.</p>";
+
+    // MONEY
+    const MONEY_FIELD = document.getElementById("moneyField");
+
+    if (MONEY_FIELD.checkValidity() === false) {
+        OUTPUT.innerHTML += "<p>Please enter a valid amount of money.</p>";
+        return;
+    }
+
+    money = Number(MONEY_FIELD.value);
+
+    if (money < 0) {
+        OUTPUT.innerHTML += "<p>You can't have negative money!!</p>";
+    } else if (money > 1000) {
+        OUTPUT.innerHTML += "<p>You are a millionaire!!</p>";
+    } else if (money === 0) {
+        OUTPUT.innerHTML += "<p>You are broke!!</p>";
+    } else if (money === 1000) {
+        OUTPUT.innerHTML += "<p>Congrats on being a millionaire!!</p>";
+    }
+
+    OUTPUT.innerHTML += "<p>You have $" + money + ".</p>";
+
+    // SHOP DISPLAY
+    displayWelcome(4, 3, 2.50);
+
+    OUTPUT.innerHTML += "<p>A chocolate bar costs $4.</p>";
+
+    if (money >= 4) {
+        OUTPUT.innerHTML += "<p>You CAN afford a chocolate bar.</p>";
+    } else {
+        OUTPUT.innerHTML += "<p>You CAN'T afford a chocolate bar.</p>";
+    }
+
+    // CHANGE
+    const CHANGE_FIELD = document.getElementById("changeField");
+
+    change = money - 4;
+    CHANGE_FIELD.value = change;
+
+    OUTPUT.innerHTML += "<p>Your change is $" + change + "</p>";
+
+    // CHOCOLATE RATING
+    const CHOCOLATE_FIELD = document.getElementById("chocolateField");
+
+    let chocolateRating = [
+        "You loathe chocolate",
+        "Chocolate is meh",
+        "Chocolate is pretty good",
+        "Chocolate is the best thing EVER!!!!"
+    ];
+
+    let choice = Number(CHOCOLATE_FIELD.value);
+
+    if (choice >= 0 && choice < chocolateRating.length) {
+        OUTPUT.innerHTML += "<p>" + chocolateRating[choice] + "</p>";
+    }
+
+    // AGE CONSOLE MESSAGES
+    if (age < 20) {
+        console.log("prolly too young yo");
+    } else if (age > 20) {
+        console.log("unc");
+    } else {
+        console.log("this is good");
+    }
 }
 
- if (age < 20){
-    console.log("prolly too young yo")
-}
-if (age > 20){
-    console.log("unc")
-}
-if (age == 20){
-    console.log("this is good")
+function displayWelcome(chocolate, chips, drink) {
+    OUTPUT.innerHTML += "<p>Chocolate bar: $" + chocolate + "</p>";
+    OUTPUT.innerHTML += "<p>Chips: $" + chips + "</p>";
+    OUTPUT.innerHTML += "<p>Drink: $" + drink + "</p>";
 }
 
-function addChocolate(){
-shoppingCart.push("chocolate");
-    OUTPUT.innerHTML += "added chocolate<br>";
+function addChocolate() {
+    shoppingCart.push("Chocolate");
+    OUTPUT.innerHTML += "Added chocolate<br>";
 }
-function addChips(){
-shoppingCart.push("chips");
-    OUTPUT.innerHTML += "added chips<br>";
+
+function addChips() {
+    shoppingCart.push("Chips");
+    OUTPUT.innerHTML += "Added chips<br>";
 }
-function addDrink(){
-shoppingCart.push("drink");
-    OUTPUT.innerHTML += "added drink<br>";
+
+function addDrink() {
+    shoppingCart.push("Drink");
+    OUTPUT.innerHTML += "Added drink<br>";
 }
-function showCart(){
-}
-    OUTPUT.innerHTML += "Cart Contents:<br>";
-for (let i=0; i < shoppingCart.length; i++){
-    OUTPUT.innerHTML += shoppingCart[i]+"<br>";
-}
+
+function showCart() {
+
+    OUTPUT.innerHTML += "<br><strong>Cart Contents:</strong><br>";
+
+    if (shoppingCart.length === 0) {
+        OUTPUT.innerHTML += "Cart is empty.<br>";
+        return;
+    }
+
+    for (let i = 0; i < shoppingCart.length; i++) {
+        OUTPUT.innerHTML += shoppingCart[i] + "<br>";
+    }
 }
